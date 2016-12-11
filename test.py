916369857822308c8b253f38febe9d4c1fbbf632
Evan Hernandez <evanfredhernandez@gmail.com>
2016-12-11 15:43:07 -0600

    Simple tests for toolbox code.

---
+++ test.py
@@ -0,0 +1,15 @@
+"""Sanity checks for codelab solutions."""
+import numpy as np
+import toolbox as tb
+
+# Test k-rank approximate.
+approx = tb.k_rank_approximate(np.identity(4), 3)
+print approx
+
+# Test train_ls_classifier.
+A = np.mat([[1, 2, 3, 4],
+            [4, 5, 6, 7],
+            [7, 8, 9, 11]])
+y = np.mat([[1], [-1], [1]])
+LS = tb.train_ls_classifier(A, y)
+print LS
