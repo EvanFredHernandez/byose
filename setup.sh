571dac4d080cdf2c88c2f80c242045d63def2a7f
Evan Hernandez <evanfredhernandez@gmail.com>
2016-12-13 19:52:15 -0600

    Remove setup.sh

--- setup.sh
+++
@@ -1,5 +0,0 @@
-sudo easy_install python pip
-sudo pip install dill nltk sklearn numpy
-python -m nltk.downloader punkt reuters stopwords
-echo Preloading the vectorized Reuters-21578 corpus. This will take a while...
-python -c 'import pre; pre.precompute_vectorized_corpus()'
