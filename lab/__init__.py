b08960c912b2daf52e145861c32a6ac09acfeb94
Evan Hernandez <evanfredhernandez@gmail.com>
2016-12-15 23:31:17 -0600

    Remove old directories.

--- lab/__init__.py
+++
@@ -1,5 +0,0 @@
-"""This code is logistical.
-
-(!!!) DO NOT CHANGE ANY CODE IN THIS FILE. (!!!)
-"""
-__all__ = ['part1', 'part2', 'part3', 'part4']
--- solutions/__init__.py
+++
@@ -1,5 +0,0 @@
-"""This code is logistical.
-
-(!!!) DO NOT CHANGE ANY CODE IN THIS FILE. (!!!)
-"""
-__all__ = ['part1_sols', 'part2_sols', 'part3_sols', 'part4_sols']
